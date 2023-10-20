58345f182c55830136b118020a402acfc3f3d88c
freelancer-g3ms <freelancer@getg3ms.com>
2023-10-20 23:48:17 +0530

    changes

--- src/client/pages/auth/login.tsx
+++ src/client/pages/auth/login.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-import Link from 'next/link'
+import Link from 'next/link';
 
 function Login() {
   return (
     <div>
-        <h1>Login Page</h1>
-        <Link href="http://localhost:5000/auth/login/google">Click here to login with Google</Link>
+      <h1>Login Page</h1>
+      <Link href="http://localhost:3000/auth/login/google">
+        Click here to login with Google
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/client/pages/auth/signup.tsx
+++ src/client/pages/auth/signup.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-import Link from 'next/link'
+import Link from 'next/link';
 
 function SignUp() {
   return (
     <div>
-        <h1>SignUp Page</h1>
-        <Link href="http://localhost:5000/auth/login/google">Click here to sign up with Google</Link>
+      <h1>SignUp Page</h1>
+      <Link href="http://localhost:3000/auth/login/google">
+        Click here to sign up with Google
+      </Link>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
